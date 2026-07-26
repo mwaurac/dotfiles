b353d514f1b0d4819c59772c7bb3f33b7ec7034a
Collins Mwaura <cmwambui@proton.me>
2026-07-26 21:50:18 +0300

    basic autostart scripts

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,3 +1,4 @@
+require("modules.autostart")
 require("modules.binds")
 require("modules.core")
 require("modules.monitors")
---
+++ hypr/modules/autostart.lua
@@ -0,0 +1,6 @@
+hl.on("hyprland.start", function()
+	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("hypridle")
+end)
