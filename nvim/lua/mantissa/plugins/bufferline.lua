4d64790660625596c2268bcd1733c4d97df3c452
Collins Mwaura <cmwambui@proton.me>
2026-08-17 13:43:43 +0300

    add file explorer and buffer tabs

---
+++ nvim/lua/mantissa/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+
+		opts = {
+			options = {
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true,
+					},
+				},
+			},
+		},
+	},
+}
